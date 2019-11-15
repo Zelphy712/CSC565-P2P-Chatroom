7a6c09d1c8244cde0bc3f5744dd5d58bf94e9e1a
HoviJ <jkh12@live.missouristate.edu>
2019-11-14 20:16:17 -0600

    changed from SOCK_STREAM to SOCK_DGRAM in client

--- pi_client.cpp
+++ pi_client.cpp
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
     struct sockaddr_in serv_addr;
     const char *hello = "Hello from client";
     char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
         return -1;
@@ -23,7 +23,7 @@ int main(int argc, char const *argv[])
     serv_addr.sin_port = htons(PORT);
 
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "10.13.51.152", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, "10.13.46.233", &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
