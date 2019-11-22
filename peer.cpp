285110b91825ca12c29ce26b489d0e54b6613f33
HoviJ <jkh12@live.missouristate.edu>
2019-11-21 18:58:35 -0600

    Update peer.cpp
    Added initial code for the createRoom function

--- peer.cpp
+++ peer.cpp
@@ -1,9 +1,67 @@
+#include <pthread.h>
+
+#define MAXLINE 2048
+
 void createRoom(string room_name, string password){
-    /*
-    -Send the room name, and the ip address of the peer that created it
-    -Start server function
-    */
-    return;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    const string *creation_message = "@" + room_name;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "10.13.46.233", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , creation_message , strlen(creation_message) , 0 );
+    printf("Creation message sent\n");
+    valread = read( sock , buffer, 1024);
+        struct sockaddr_in servaddr, cliaddr;
+
+    // Creating room server
+    int sockfd;
+
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&servaddr, 0, sizeof(servaddr));
+    memset(&cliaddr, 0, sizeof(cliaddr));
+
+    // Filling server information
+    servaddr.sin_family = AF_INET; // IPv4
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    // Bind the socket with the server address
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    int n;
+    socklen_t len;
+    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len);
+        printf("test\n");
+
+
 }
 
 void joinRoom(string room_name, string password) {
@@ -38,6 +96,10 @@ void broadcastMessage(string message){
     -Broadcast message to all peers
     */
     return;
+}
+
+int main(int argc, char* argv[]){
+    string pi_server = "some ip address in dotted decimal form";
 }
 
 
