b294334bd14637f676baae36f2448141e203a965
HoviJ <jkh12@live.missouristate.edu>
2019-12-02 17:59:22 -0600

    Create users in a room
    Room server can map usernames to ip structures

--- peer.cpp
+++ peer.cpp
@@ -1,5 +1,6 @@
 #include <pthread.h>
 #include <mutex>
+#include <map>
 #include <cstring>
 #include <iostream>
 #include <sys/types.h>
@@ -24,19 +25,42 @@ struct room_args {
     string room_name;
     string password;
 };
+int i = 0;
 int is_server = 0;
 const char pi_server[] = "10.13.49.208";
-struct sockaddr_in room_addr[100];
+string username;
+map <string, struct sockaddr_in> room_addr;
 
 void *sendMessage(void*){
+    int sock = 0;
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
+        printf("\n Socket creation error \n");
+    }
     while(true){
         string message;
         output_lock.lock();
         cout << "Please input your message: ";
         cin >> message;
         output_lock.unlock();
+        if(is_server){
+            char server_message[message.size() +1];
+            strcpy(server_message, message.c_str());
+            for(map<string, struct sockaddr_in>::iterator it = room_addr.begin(); it!=room_addr.end(); ++it){
+                if (connect(sock, (struct sockaddr *)&it->second, sizeof(it->second)) < 0){
+                    printf("\nConnection Failed \n");
+                }
+                cout << to_string(it->second.sin_addr.s_addr);
+                send(sock , server_message , strlen(server_message) , 0 );
+
+                bzero(server_message, sizeof(server_message));
+            }
+        }
+        else{
+            // Send message to the room server
+        }
 
     }
+    pthread_exit(NULL);
 }
 /*
 /****************************************************************
@@ -78,10 +102,8 @@ void contactPiServer(string message) {
     }
     socklen_t len_server = sizeof(serv_addr);
     send(sock , server_message , strlen(server_message) , 0 );
-    cout << "Message sent" << endl;
     bzero(server_message, sizeof(server_message));
-    recvfrom(sock, (char *)buffer, sizeof(buffer), MSG_WAITALL, ( struct sockaddr *) &serv_addr, &len_server);
-    cout << buffer << endl;
+    recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &serv_addr, &len_server);
 
     close(sock);
 }
@@ -101,7 +123,9 @@ void *startRoomServer(void* arguments){
     memset(&cliaddr, 0, sizeof(cliaddr));
 
     socklen_t cli_len = sizeof(cliaddr);
-    // Filling server information
+    // Filling server information        int sock = 0, valread;
+
+    socklen_t len_client;
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(12001);
@@ -112,13 +136,33 @@ void *startRoomServer(void* arguments){
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-
-    int n;
-    output_lock.lock();
-    cout << "Room: " << room->room_name << endl;
-    output_lock.unlock();
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &cli_len);
     is_server = 1;
+    int n;
+
+    recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &cliaddr, &len_client);
+
+    if(string(buffer).at(0) == '@'){
+        char already_in_use[4];
+        if(room_addr.count(string(buffer).substr(1)) > 0){
+            strcpy(already_in_use, "1");
+            if(sendto(sockfd, already_in_use, sizeof(already_in_use), 0, (const struct sockaddr*)&cliaddr, len_client) < 0){
+                perror("Send failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else{
+            room_addr.insert(pair<string,struct sockaddr_in>(string(buffer).substr(1),cliaddr));
+            const char* message = to_string(room_addr[string(buffer).substr(1)].sin_port).c_str();
+            if(sendto(sockfd, message, sizeof(message), 0, (const struct sockaddr*)&cliaddr, len_client) < 0){
+                perror("Send failed");
+                exit(EXIT_FAILURE);
+            }
+        }
+        strcpy(already_in_use, "");
+
+    }
+
+
     pthread_exit(NULL);
 
 }
@@ -148,11 +192,46 @@ void joinRoom() {
     */
 
     string name;
+    string password;
     cout << "What room do you want to join? ";
     cin >> name;
     cout << endl;
+    int sock = 0, valread;
 
-    contactPiServer("?"+name);
+    socklen_t len_client;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(12001);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, pi_server, &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+    }
+
+    cout << "Enter a username: ";
+    cin >> username;
+
+    string message = "@" + username;
+    char server_message[message.size() +1];
+    strcpy(server_message, message.c_str());
+    socklen_t len_server = sizeof(serv_addr);
+    send(sock , server_message , strlen(server_message) , 0 );
+    bzero(server_message, sizeof(server_message));
+    recvfrom(sock, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr *) &serv_addr, &len_server);
+    cout << buffer << endl;
+    //contactPiServer("?"+name);
 
     return;
 }
@@ -170,7 +249,7 @@ void exitRoom() {
 
 
 int main(){
-    int choice;
+    int choice;output_lock.unlock();
 
     while(true){
         cout << "Choose an option:" << endl << "1. Create a room" << endl << "2. Join a room" << endl;
